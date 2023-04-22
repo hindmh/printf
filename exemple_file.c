20326442199b5a428ff9650dba835d5698885e79
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-22 06:31:22 -0700

    second updating to avoid asking for username and password each time i m pushing somthing

--- exemple_file.c
+++ exemple_file.c
@@ -5,4 +5,5 @@ void main (void)
 {
   printf (" hello hind, try to get this modification ");
   printf(" i got the solution, it s about taking new personnal acces token only ");
+  printf(" an other trial for not repeatng the username and password each time i m pushing some updating");
 }

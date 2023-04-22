5157225e282548684eae5097175e5c653e9887e8
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-22 14:33:22 -0700

    removing inutil fles

--- i_resolve_it_hind.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-void main (void)
-{
-	printf(" Bonjour Hind finally i adjust this problem of authentifaction each time i want to push modification ");
-}

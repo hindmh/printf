8fdd71813e3b6b49c2602ba5f7c8d03fb3357862
abdelkhalk tahiri <abdelkhalk.tahiri@gmail.com>
2023-04-22 07:28:41 -0700

    it s ok now

--- file_test.c
+++
@@ -1,8 +0,0 @@
-#include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
-void main (void)
-{
-	printf(" Bonjour ");
-}
---
+++ i_resolve_it_hind.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void main (void)
+{
+	printf(" Bonjour Hind finally i adjust this problem of authentifaction each time i want to push modification ");
+}
